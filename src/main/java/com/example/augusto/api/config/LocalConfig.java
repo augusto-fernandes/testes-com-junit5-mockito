9923f37d4c1e3fce5625ba2b1e0b568d0014d050
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-07-04 14:13:25 -0300

    perfil de testes

---
+++ src/main/java/com/example/augusto/api/config/LocalConfig.java
@@ -0,0 +1,26 @@
+package com.example.augusto.api.config;
+
+import com.example.augusto.api.domain.User;
+import com.example.augusto.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Bean
+    public void startDB(){
+        User u1 = new User(null,"Augusto", "augusto@gmail.com","123");
+        User u2 = new User(null,"joao", "joao@gmail.com","1234");
+
+        repository.saveAll(List.of(u1,u2));
+    }
+}
