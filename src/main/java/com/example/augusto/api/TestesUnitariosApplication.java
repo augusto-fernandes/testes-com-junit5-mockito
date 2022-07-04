685e337291d8842c44a5003c3329d4f0db7ae4b3
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-07-04 18:32:52 -0300

    endpoint findAll para user

--- src/main/java/com/example/augusto/api/TestesUnitariosApplication.java
+++ src/main/java/com/example/augusto/api/TestesUnitariosApplication.java
@@ -2,6 +2,7 @@ package com.example.augusto.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class TestesUnitariosApplication {
--- src/main/java/com/example/augusto/api/services/UserService.java
+++ src/main/java/com/example/augusto/api/services/UserService.java
@@ -2,8 +2,9 @@ package com.example.augusto.api.services;
 
 import com.example.augusto.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
-
-
     User findById(Integer id);
+    List<User> findAll();
 }
