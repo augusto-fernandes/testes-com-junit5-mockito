f5166ea5343b8835d0f123b6f97cc16703435e4b
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-07-06 18:46:18 -0300

    teste para classe user serviceimpl metodo create com sucesso

--- src/test/java/com/example/augusto/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/example/augusto/api/services/impl/UserServiceImplTest.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -90,11 +91,20 @@ class UserServiceImplTest {
         assertEquals(EMAIL, response.get(INDEX).getEmail());
         assertEquals(PASSWORD, response.get(INDEX).getPassword());
 
-
     }
 
     @Test
-    void create() {
+    void whenCreateTheReturnSucces() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
